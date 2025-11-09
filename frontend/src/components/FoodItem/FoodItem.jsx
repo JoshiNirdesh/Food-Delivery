0ed8b9eab0c84282f3145b156d60b6ab11930d16
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-11-09 13:37:22 +0545

    responsive design

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,30 +1,29 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./fooditem.css";
 import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 const FoodItem = ({ id, name, price, description, image }) => {
-  const [itemCount, setItemCount] = useState(0);
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img className="food-item-image" src={image} alt="" />
-        {!itemCount ? (
+        {!cartItems[id] ? (
           <img
-            onClick={() => {
-              setItemCount((prev) => prev + 1);
-            }}
+            onClick={() => addToCart(id)}
             src={assets.add_icon_white}
             className="add"
           />
         ) : (
           <div className="food-item-counter">
             <img
-              onClick={() => setItemCount((prev) => prev - 1)}
+              onClick={() => removeFromCart(id)}
               src={assets.remove_icon_red}
               alt=""
             />
-            <p>{itemCount}</p>
+            <p>{cartItems[id]}</p>
             <img
-              onClick={() => setItemCount((prev) => prev + 1)}
+              onClick={() => addToCart(id)}
               src={assets.add_icon_green}
               alt=""
             />
